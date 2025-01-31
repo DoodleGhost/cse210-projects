fabc40e540604516092fccd55b33bf7565c106f6
DoodleGhost <joshualogancorey@gmail.com>
2025-01-30 19:10:28 -0700

    Finishing Resume Project

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -18,8 +18,14 @@ class Program
         job2._startYear = 2022;
         job2._endYear = 2023;
 
-        job1.Display();
-        job2.Display();
+        // Create a resume object
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 
 
